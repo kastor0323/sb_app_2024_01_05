50c6ca0f7a73743404e5e02650b34ae94ccbbe70
kastor0323 <kastor0323@gmail.com>
2024-01-24 14:32:55 +0900

    호출시 반환되는 값이 1씩 커지는 컨트롤러 액션 구현

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/home/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/home/controller/UsrHomeController.java
@@ -4,11 +4,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-public class UsrHomeController {
- @RequestMapping("/usr/home/main")
-  @ResponseBody
-  public String showmain(){
-   return  "안녕하세요";
+@Controller //주석 특징이 new 객체 생성을 안하고 객체를 만들 수 있음
+public class UsrHomeController { //일반 사용자가 이용하는 컨트롤러
+ private int count;
+
+ public  UsrHomeController(){
+  count = 0;
  }
+  @RequestMapping("/usr/home/main")
+  @ResponseBody
+  public String showMain(){
+   return  "안녕하세요";
+  }
+
+ @RequestMapping("/usr/home/main2")
+ @ResponseBody
+ public String showMain2(){
+  return  "반갑습니다.";
+ }
+
+ @RequestMapping("/usr/home/main3")
+ @ResponseBody
+ public String showMain3(){
+  return  "또 만나요";
+ }
+
+ @RequestMapping("/usr/home/main4")
+ @ResponseBody
+ public int showMain4(){
+  return ++count;
+ }
+
 }
